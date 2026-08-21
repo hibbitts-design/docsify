cfc6b91eaafc67a138c30c788babbd1c290bb9d9
Paul Hibbitts <paul@hibbittsdesign.org>
2026-08-21 07:00:09 -0700

    Revert "feat(sidebar): add collapsible root sidebar groups (#2784)"
    This reverts commit 50992940f32b98469c2ffae5dddaa06c2927e98e.

--- src/core/config.js
+++ src/core/config.js
@@ -11,7 +11,6 @@ const defaultDocsifyConfig = () => ({
   autoHeader: false,
   basePath: '',
   catchPluginErrors: true,
-  collapseSidebarGroups: false,
   cornerExternalLinkTarget:
     /** @type {'_blank' | '_self' | '_parent' | '_top'  | '_unfencedTop'} */ (
       '_blank'
